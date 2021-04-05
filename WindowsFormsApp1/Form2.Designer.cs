f6126107ba7126265ddd4c3967b265d30aaabeae
kammarx <kammarufa6@gmail.com>
2021-04-06 03:01:19 +0500

    Deleted Button to Form3

---
+++ WindowsFormsApp1/Form2.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+      
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(196, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "question";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(186, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(186, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(186, 338);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 30);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(403, 388);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 18);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(338, 388);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Счет:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(12, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 18);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Вопрос №";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(93, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(16, 18);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "1";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(12, 367);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 45);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Меню";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(337, 300);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 24);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "30";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(23, 213);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "label7";
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button5.BackgroundImage")));
+            this.button5.Location = new System.Drawing.Point(406, 26);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(58, 48);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form2
+            // 
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(530, 458);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Lable1()
+        {
+            
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button4;
+        private Timer timer1;
+        private Label label6;
+        private Label label7;
+        private Button button5;
+    }
+}
---
+++ WindowsFormsApp1/Form2.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Windows.Threading;
+using System.Media;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+       
+        public Form2()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        StreamReader sr = new StreamReader("Sample.txt");
+       
+        int i, r1, r2 = 0; // i - номер вопроса, связан с очками, r1, r2 - рандомные числа для рандомизации расположения ответов между кнопками
+        int h = 0,numOfСlicks=0;
+        int m = 8; // кол-во вопросов минус 1 , используется для рандомизации выдачи вопросов 
+        int c = 1; // счетчик номера вопроса
+        int imax = 9; // сколько всего вопросов 
+        int timeLeft = 30; // Для таймера
+        Random r = new Random(DateTime.Now.Millisecond); // подключаем рандом для r
+        string[] q = { "5+1", "4+4", "5-2", "3*5", "6*7", "4*9", "144/12", "10^2", "2*2" }; // массив с вопросами
+        string[] a1 = { "6", "8", "3", "15", "42", "36", "12", "100", "4" };   // массив правильных ответов
+        string[] a2 = { "23", "33", "123", "17", "49", "22", "11", "1024", "5" };   // массив неправильных ответов
+        string[] a3 = { "58", "31", "44", "21", "32", "51", "14", "1000", "6" };   // массив неправильных ответов
+
+        void Endgame(int e)
+        {
+            if (e == 0)
+                label1.Text = "конец игры";
+            else
+            {
+                label2.Text = (c).ToString(); c++;
+                label1.Text = "игра пройдена";
+            }
+            timeLeft = 0;
+            label6.Visible = false;
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+        }
+        private object Otvet(int o)     //запускается при нажатии кнопок
+        {
+            Button[] b1 = { button1, button2, button3 };
+            if (c != imax) // проверка не кончились ли вопросы
+            {
+                if (b1[o].Text == a1[i]) // если выбран правильный ответ 
+                {
+                    timeLeft = 31;
+                    label4.Text = "Вопрос №"; c++;
+                    label5.Text = c.ToString();
+                    label2.Text = (c - 1).ToString();
+                    q[i] = q[m];  // на место i-ого вопроса записывается вопрос с конца, i-ый пропадает 
+                    a1[i] = a1[m]; // то же самое с ответами
+                    a2[i] = a2[m];
+                    a3[i] = a3[m];
+                    m--;
+                    i = r.Next(0, m); // рандомизируем i от 0 до текущего m невключительно
+                    label1.Text = q[i];
+                    string[][] a = { a1, a2, a3 };
+                    r1 = r.Next(0, 3);
+                    r2 = r.Next(0, 2);
+                    for (int j = 0; j < 3; j++)
+                    {
+                        b1[j].Text = a[(r1 == j) ? 2 : ++r2 % 2][i];
+                    }
+                }
+                else
+                    Endgame(0);
+            }
+            else
+            {
+                if (b1[o].Text == a1[i])
+                    Endgame(1);
+                else
+                    Endgame(0);
+            }
+            return 0;
+        }
+        public Form2(Form1 f1)
+        {
+            InitializeComponent(); //  связь 1-ой и 2-ой форм
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void Form2_Load_1(object sender, EventArgs e)
+        {
+            Button[] b = { button1, button2, button3 };
+            label7.Text = h.ToString();
+            string[][] a = { a1, a2, a3 };
+            i = r.Next(0, m - 1); // рандомизируем i
+            label1.Text = q[i];  // в надпись для вопроса записывается i-ый вопроc
+            r1 = r.Next(0, 3);
+            r2 = r.Next(0, 2);
+            for (int j = 0; j < 3; j++)
+            {
+                b[j].Text = a[(r1 == j) ? 2 : ++r2 % 2][i];
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Otvet(0);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Otvet(1);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            numOfСlicks++;
+            SoundPlayer s = new SoundPlayer();
+            s.SoundLocation = @".\Background_music.wav";
+            if (numOfСlicks % 2 != 0) s.Stop();
+            else s.PlayLooping();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Otvet(2);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft = timeLeft - 1;
+                label6.Text = timeLeft + "";
+            }
+            else
+            {
+                timer1.Stop();
+                Endgame(0);
+            }
+        }
+    }
+}
+
+        
+    
+
